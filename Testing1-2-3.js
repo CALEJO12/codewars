59d2584dff2fb9b20de279994a12e2d1c5b3ef8f
Cody Alejo <cody.alejo1997@gmail.com>
2022-05-11 22:01:27 -0500

    Simplified codewars solution.

--- Testing1-2-3.js
+++ Testing1-2-3.js
@@ -1,7 +1,3 @@
 var number=function(array){
-    let newArray = []
-    for(let i = 1; i < array.length + 1; i++) {
-      newArray.push(`${i}: ${array[i-1]}`)
-    }
-    return newArray
-  }
+  return array.map( (element, index) => `${index+1}: ${element}`)
+}
