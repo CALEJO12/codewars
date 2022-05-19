480fe9f72d9c5dceb4b115be102771a479bab768
Cody Alejo <cody.alejo1997@gmail.com>
2022-05-18 20:49:39 -0500

    updated solution with includes method

--- everyNumberExceptFive.js
+++ everyNumberExceptFive.js
@@ -1,8 +1,7 @@
 function dontGiveMeFive(start, end){
     let result = 0;
     for(let i = start; i <= end; i++) {
-      if(i % 5 !== 0 || i === 10) {
-        console.log(i)
+      if(!i.toString().includes('5')) {
         result++
       }
     }
